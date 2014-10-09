eeeb66072cf0e6f557228397528c9be6c29fa880
joaoprudencio <joao.prudencio@blip.pt>
2014-10-09 10:42:00 +0100

    refactoring

--- RSSReaderSwift/RSSReaderSwift/ArticleDataSource.swift
+++ RSSReaderSwift/RSSReaderSwift/ArticleDataSource.swift
@@ -3,7 +3,7 @@
 //  RSSReaderSwift
 //
 //  Created by joaoprudencio on 22/09/14.
-//  Copyright (c) 2014 Blip. All rights reserved.
+//  Copyright (c) 2014 joaoprudencio. All rights reserved.
 //
 
 import UIKit
