29f7522c66c66f1e86f1d2a6f85f6a066f4d868e
joaoprudencio <joao.prudencio@blip.pt>
2014-10-17 23:00:40 +0100

    started testing NSURLSessionDataTask

--- RSSReaderSwift/RSSReaderSwift/FeedManager.swift
+++ RSSReaderSwift/RSSReaderSwift/FeedManager.swift
@@ -12,7 +12,7 @@ class FeedManager {
     
     func load(closure: (Array<Article>) -> ()) {
         
-        let url = NSURL(string: "https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q=https://betting.betfair.com/index.xml&num=20")
+        let url = NSURL(string: "https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q=http://betting.betfair.com/index.xml&num=20")
         let request = NSURLRequest(URL: url!)
         
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
@@ -24,5 +24,21 @@ class FeedManager {
             
             closure(articles)
         }
+        
+        // TODO refactor to use NSURLSessionDataTask
+
+       /* let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            
+            var jsonErrorOptional: NSError?
+            let jsonOptional: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &jsonErrorOptional)
+            
+            var articles = FeedParser.parseFeed(jsonOptional)
+            
+            closure(articles)
+        })
+        
+        task.resume()
+        */
     }
 }
