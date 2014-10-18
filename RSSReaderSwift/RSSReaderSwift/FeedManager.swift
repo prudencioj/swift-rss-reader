f774edaf5f3f68228c3cd2f0666d1716b2de5e57
joaoprudencio <joao.prudencio@blip.pt>
2014-10-18 14:21:18 +0100

    start using NSURLSession to fetch the feed

--- RSSReaderSwift/RSSReaderSwift/FeedManager.swift
+++ RSSReaderSwift/RSSReaderSwift/FeedManager.swift
@@ -10,35 +10,32 @@ import Foundation
 
 class FeedManager {
     
+    // class variables are not supported yet
+    let feedUrl = "https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q=http://betting.betfair.com/index.xml&num=20"
+    
     func load(closure: (Array<Article>) -> ()) {
         
-        let url = NSURL(string: "https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q=http://betting.betfair.com/index.xml&num=20")
+        let url = NSURL(string: feedUrl)
         let request = NSURLRequest(URL: url!)
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-            
-            var jsonErrorOptional: NSError?
-            let jsonOptional: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &jsonErrorOptional)
-            
-            var articles = FeedParser.parseFeed(jsonOptional)
-            
-            closure(articles)
-        }
-        
-        // TODO refactor to use NSURLSessionDataTask
-
-       /* let session = NSURLSession.sharedSession()
+        let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
             
             var jsonErrorOptional: NSError?
             let jsonOptional: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &jsonErrorOptional)
             
-            var articles = FeedParser.parseFeed(jsonOptional)
+            // parse the json feed
             
-            closure(articles)
+            let articles = FeedParser.parseFeed(jsonOptional)
+            
+            // return articles in the main thread
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                
+                closure(articles)
+            })
         })
         
         task.resume()
-        */
     }
 }
